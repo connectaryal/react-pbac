8b4739d85fd0f6ee2f821dfb601f8ecf5e551c7f
Shiva Aryal <shiva.aryal@intuji.com>
2025-09-22 22:32:02 +0545

    feat: update exports in index.ts to use PBACCore instead of PBAC

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // Root directory for the library
 // Export core classes and types
-export { PBAC } from "./core/pbac";
+export { PBACCore } from "./core/pbac";
 export * from "./types";
 
 // Re-export for convenience
-export { PBAC as default } from "./core/pbac";
+export { PBACCore as default } from "./core/pbac";
 
 // Export React components and hooks
 export {
